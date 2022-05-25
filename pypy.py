f2d7dee3b73fdf033bb04692f9f3497a0947f15a
Joao Victor Lopes Pereira dos Santos <13672982@lab600-15.dcm.ffclrp.usp.br>
2022-05-25 17:56:46 -0300

    casellato e gado

---
+++ pypy.py
@@ -0,0 +1,24 @@
+casellato = ['Emocionado','Burro','Mongoloide','Simp']
+respostaSim = ['Sim', 'sim']
+respostaNao = ['Não', 'não', 'Nao', 'nao']
+print('Lista de coisas que o Casellato é: ')
+for i in casellato:
+    if (i <= casellato[2]):
+        print(i + ', ', end='')
+    if (i == casellato[3]):
+        print(i + '.')
+
+resposta = input('\nCasellato, vc vai parar de ser gadinho de mulher?: ')
+if (resposta in respostaSim):
+    casellato.pop(3)
+    casellato.pop(1)
+    print('\nFinalmente')
+    print("\nLista de coisas que o Casellato é: " + str(casellato[0]) +" ," + str(casellato[1]) + ", Menos Burro.")
+
+else:
+   if (resposta in respostaNao):          
+      print('\nEu te odeio muito')      
+      print('\nLista de coisas que o Casellato é: ' + str(casellato[0]) + ', '
+       + str(casellato[2]) + ', ' + str(casellato[3]) + ", Muito Burro.")
+
+    
